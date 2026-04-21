e40f29b99fb6d6c68f06730f007dcd73c8433bf4
James Armstrong <arsngrobg@gmail.com>
2026-04-22 00:15:18 +0100

    fix: calling options 'flags'

--- libcc.h
+++ libcc.h
@@ -33,14 +33,14 @@ void          cc_delete(CC_Toolchain *cc);
 // Operations
 void        cc_set_invocation_style(CC_Toolchain *cc, CC_InvocationStyle style);
 bool        cc_set_profile         (CC_Toolchain *cc, const char *ccid, CC_InvocationStyle style);
-bool        cc_set_compiler        (CC_Toolchain *cc, const char *ccid); // [<CCID>]
-bool        cc_add_flag            (CC_Toolchain *cc, const char *flag); // cc    [<FLAG>]
-bool        cc_add_include_path    (CC_Toolchain *cc, const char *path); // cc -I[<PATH>]
-bool        cc_add_library_path    (CC_Toolchain *cc, const char *path); // cc -L[<PATH>]
-bool        cc_add_library         (CC_Toolchain *cc, const char *lib);  // cc -l[<LIB>]
-bool        cc_add_source          (CC_Toolchain *cc, const char *file); // cc    [<FILE>]
-bool        cc_set_output          (CC_Toolchain *cc, const char *out);  // cc -o [<OUT>]
-const char *cc_render_command      (CC_Toolchain *cc);                   // [<CCID>] [<FLAG>]* (-I[<PATH>])* (-L[<PATH>])* (-l[<LIB>])* [<FILE>]+ -o [<OUT>]
+bool        cc_set_compiler        (CC_Toolchain *cc, const char *ccid);   // [<CCID>]
+bool        cc_add_option          (CC_Toolchain *cc, const char *option); // cc    [<FLAG>]
+bool        cc_add_include_path    (CC_Toolchain *cc, const char *path);   // cc -I[<PATH>]
+bool        cc_add_library_path    (CC_Toolchain *cc, const char *path);   // cc -L[<PATH>]
+bool        cc_add_library         (CC_Toolchain *cc, const char *lib);    // cc -l[<LIB>]
+bool        cc_add_source          (CC_Toolchain *cc, const char *file);   // cc    [<FILE>]
+bool        cc_set_output          (CC_Toolchain *cc, const char *out);    // cc -o [<OUT>]
+const char *cc_render_command      (CC_Toolchain *cc);                     // [<CCID>] [<FLAG>]* (-I[<PATH>])* (-L[<PATH>])* (-l[<LIB>])* [<FILE>]+ -o [<OUT>]
 int         cc_invoke              (CC_Toolchain *cc);
 
 #endif // LIBCC_H
