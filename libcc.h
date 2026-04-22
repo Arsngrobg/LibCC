6bba0128a9a3964660e7acb9b2cb207f4ae6029d
James Armstrong <arsngrobg@gmail.com>
2026-04-23 00:35:00 +0100

    refactor: types & docs

--- libcc.h
+++ libcc.h
@@ -17,6 +17,7 @@
 #define CC_PROFILE_CLANG    (CC_COMPILER_CLANG), (CC_STYLE_GNU)
 #define CC_PROFILE_MSVC     (CC_COMPILER_MSVC),  (CC_STYLE_MSVC)
 
+#include <stdint.h>
 #include <stdbool.h>
 
 typedef enum {
@@ -31,16 +32,16 @@ CC_Toolchain *cc_new   (void);
 void          cc_delete(CC_Toolchain *cc);
 
 // Operations
-void        cc_set_invocation_style(CC_Toolchain *cc, CC_InvocationStyle style);
 bool        cc_set_profile         (CC_Toolchain *cc, const char *ccid, CC_InvocationStyle style);
+bool        cc_set_invocation_style(CC_Toolchain *cc, CC_InvocationStyle style);
 bool        cc_set_compiler        (CC_Toolchain *cc, const char *ccid);   // [<CCID>]
-bool        cc_add_option          (CC_Toolchain *cc, const char *option); // cc    [<FLAG>]
+bool        cc_add_option          (CC_Toolchain *cc, const char *option); // cc    [<OPTION>]
 bool        cc_add_include_path    (CC_Toolchain *cc, const char *path);   // cc -I[<PATH>]
 bool        cc_add_library_path    (CC_Toolchain *cc, const char *path);   // cc -L[<PATH>]
 bool        cc_add_library         (CC_Toolchain *cc, const char *lib);    // cc -l[<LIB>]
 bool        cc_add_source          (CC_Toolchain *cc, const char *file);   // cc    [<FILE>]
 bool        cc_set_output          (CC_Toolchain *cc, const char *out);    // cc -o [<OUT>]
-const char *cc_render_command      (CC_Toolchain *cc);                     // [<CCID>] [<FLAG>]* (-I[<PATH>])* (-L[<PATH>])* (-l[<LIB>])* [<FILE>]+ -o [<OUT>]
-int         cc_invoke              (CC_Toolchain *cc);
+const char *cc_render_command      (CC_Toolchain *cc);                     // [<CCID>] [<OPTION>]* (-I[<PATH>])* (-L[<PATH>])* (-l[<LIB>])* [<FILE>]+ -o [<OUT>]
+int32_t     cc_invoke              (CC_Toolchain *cc);
 
 #endif // LIBCC_H
