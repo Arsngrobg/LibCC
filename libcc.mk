b1be09a0947c4b26e46b31f0e442184e92615035
James Armstrong <arsngrobg@gmail.com>
2026-04-24 16:02:34 +0100

    fix(build): root dir for accurate object seperation

--- libcc.mk
+++ libcc.mk
@@ -7,37 +7,38 @@
 #      LibCC - lightweight C compiler invocation library
 
 # PROJECT
-NAME     := libcc
+NAME      := libcc
 
 # COMPILATION & INSTALLATION
-CC       ?= cc
-CFLAGS   ?= -std=c99 -O2 -Wall -Wextra -Wpedantic
-AR       ?= ar
-ARFLAGS  := -rcs
-PREFIX   ?= /usr/local
+CC        ?= cc
+CFLAGS    ?= -std=c99 -O2 -Wall -Wextra -Wpedantic
+AR        ?= ar
+ARFLAGS   := -rcs
 
 # BUILD SYSTEM
-BUILDDIR ?= build
-OBJDIR   := $(BUILDDIR)/obj
-LIBDIR   := $(BUILDDIR)/lib
+LIBCCROOT ?= $(dir $(lastword $(MAKEFILE_LIST)))
+LIBCCOBJ  ?= $(notdir $(patsubst %/,%,$(LIBCCROOT)))
+BUILDDIR  ?= build
+OBJDIR    := $(BUILDDIR)/obj
+LIBDIR    := $(BUILDDIR)/lib
 
 # COMPATABILITY
-MACHINE  := $(shell $(CC) -dumpmachine)
+MACHINE   := $(shell $(CC) -dumpmachine)
 ifneq ($(findstring mingw,$(MACHINE)),)
-MKDIR    := mkdir
-RM       := rmdir /S /Q 2>nul
-SOEXT    := dll
-SOFLAG   := -shared
+MKDIR     := mkdir
+RM        := rmdir /S /Q 2>nul
+SOEXT     := dll
+SOFLAG    := -shared
 else
-MKDIR    := mkdir -p
-RM       := rm -rf
+MKDIR     := mkdir -p
+RM        := rm -rf
 ifneq ($(findstring darwin,$(MACHINE)),)
-SOEXT    := dylib
-SOFLAG   := -dynamiclib
+SOEXT     := dylib
+SOFLAG    := -dynamiclib
 endif
 override CFLAGS += -fPIC
-SOEXT    := so
-SOFLAG   := -fPIC -shared
+SOEXT     := so
+SOFLAG    := -shared
 endif
 
 # RECIPES
@@ -49,7 +50,7 @@ $(LIBDIR)/$(NAME).a: $(OBJDIR)/$(NAME).o | $(LIBDIR)/
 	@echo [Make] Creating $(NAME) static library ($(NAME).a)
 	@$(AR) $(ARFLAGS) $@ $<
 
-$(OBJDIR)/$(NAME).o: $(NAME).c | $(OBJDIR)/
+$(OBJDIR)/$(NAME).o: $(LIBCCROOT)/$(NAME).c | $(OBJDIR)/
 	@echo [Make] Compiling $@
 	@$(CC) $(CFLAGS) -c $< -o $@
 
