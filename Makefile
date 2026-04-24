8e8b569a7e80831d6e794aaccf751d9b0ae43dcc
James Armstrong <arsngrobg@gmail.com>
2026-04-24 17:54:29 +0100

    fix(build): cross-platform

--- Makefile
+++ Makefile
@@ -15,8 +15,9 @@ shared: $(LIBDIR)/$(NAME).$(SOEXT)
 static: $(LIBDIR)/$(NAME).a
 
 clean:
-	@echo make: Purging $(BUILDDIR) directory
+	@$(LOG) Purging $(BUILDDIR) directory
 	@$(RM) $(subst /,\,$(BUILDDIR)) || exit 0
 
 .DEFAULT_GOAL = all
 .PHONY: all shared static clean
+	
--- libcc.mk
+++ libcc.mk
@@ -22,23 +22,32 @@ BUILDDIR  ?= build
 OBJDIR    := $(BUILDDIR)/obj
 LIBDIR    := $(BUILDDIR)/lib
 
-# COMPATABILITY
-MACHINE   := $(shell $(CC) -dumpmachine)
-ifneq ($(findstring mingw,$(MACHINE)),)
+# SHELL UTILITIES
+ifdef ComSpec # cmd.exe/Powershell.exe most likely
+FSEP      := \\
+CP        := copy /y >nul
 MKDIR     := mkdir
 RM        := rmdir /S /Q 2>nul
-SOEXT     := dll
-SOFLAG    := -shared
 else
+FSEP      := /
+CP        := cp
 MKDIR     := mkdir -p
 RM        := rm -rf
-ifneq ($(findstring darwin,$(MACHINE)),)
+endif
+LOG       := echo make:
+
+# LIBRARY FORMATTING
+MACHINE   := $(shell $(CC) -dumpmachine)
+ifneq ($(findstring mingw,$(MACHINE)),)
+SOEXT     := dll
+SOFLAG    := -shared
+else ifneq ($(findstring darwin,$(MACHINE)),)
 SOEXT     := dylib
 SOFLAG    := -dynamiclib
-endif
-override CFLAGS += -fPIC
+else
 SOEXT     := so
 SOFLAG    := -shared
+override CFLAGS += -fPIC
 endif
 
 # RECIPES
@@ -48,17 +57,17 @@ LIBCC_STATIC := $(LIBDIR)/$(NAME).a
 LIBCC_SHARED := $(LIBDIR)/$(NAME).$(SOEXT)
 
 $(LIBCC_SHARED): $(LIBCC_OBJECT) | $(LIBDIR)/
-	@echo make: creating $(NAME) shared library ($(NAME).$(SOEXT))
+	@$(LOG) Creating $(NAME) shared library ($(NAME).$(SOEXT))
 	@$(CC) $(SOFLAG) -o $@ $<
 
 $(LIBCC_STATIC): $(LIBCC_OBJECT) | $(LIBDIR)/
-	@echo make: Creating $(NAME) static library ($(NAME).a)
+	@$(LOG) Creating $(NAME) static library ($(NAME).a)
 	@$(AR) $(ARFLAGS) $@ $<
 
 $(LIBCC_OBJECT): $(LIBCC_SOURCE) | $(OBJDIR)/$(LIBCCOBJ)/
-	@echo make: Compiling $@
+	@$(LOG) Compiling $@
 	@$(CC) $(CFLAGS) -c $< -o $@
 
 %/:
-	@echo make: Creating new directory $(subst /,\,\$(patsubst %/,%,$@))
-	@$(MKDIR) $(subst /,\,$@)
+	@$(LOG) Creating new directory $@
+	@$(MKDIR) $(subst /,$(FSEP),$@)
