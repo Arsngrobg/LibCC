e0f9f60a1e699494f94551aa28fedd1c8d6b9ebe
James Armstrong <arsngrobg@gmail.com>
2026-04-25 13:00:05 +0100

    fix: pathing issues

--- libcc.mk
+++ libcc.mk
@@ -12,16 +12,13 @@ LIBCC_MK := $(lastword $(MAKEFILE_LIST))
 -include mymk/mymk.mk
 
 # RELATIVE FILE PATHS
-LIBCC_PREFIX := $(dir $(LIBCC_MK))
-ifeq ($(LIBCC_PREFIX),./)
-LIBCC_PREFIX := $()
-endif
+LIBCC_PREFIX := $(notdir $(patsubst %/,%,$(dir $(LIBCC_MK))))
 
 # RECIPES
-LIBCC_SOURCE := $(LIBCC_PREFIX)libcc.c
-LIBCC_OBJECT := $(OBJDIR)/$(LIBCC_PREFIX)libcc.o
-LIBCC_SHARED := $(LIBDIR)/$(LIBCC_PREFIX)libcc.$(SOEXT)
-LIBCC_STATIC := $(LIBDIR)/$(LIBCC_PREFIX)libcc.a
+LIBCC_SOURCE := $(LIBCC_PREFIX)/libcc.c
+LIBCC_OBJECT := $(OBJDIR)/$(LIBCC_PREFIX)/libcc.o
+LIBCC_SHARED := $(LIBDIR)/libcc.$(SOEXT)
+LIBCC_STATIC := $(LIBDIR)/libcc.a
 
 $(LIBCC_STATIC): $(LIBCC_OBJECT) | $(LIBDIR)/
 	@$(LOG) Creating libcc static library (libcc.a)
@@ -31,7 +28,7 @@ $(LIBCC_SHARED): $(LIBCC_OBJECT) | $(LIBDIR)/
 	@$(LOG) Creating libcc shared library (libcc.$(SOEXT))
 	@$(CC) $(CFLAGS) $(SOFLAG) -o $@ $<
 
-$(LIBCC_OBJECT): $(LIBCC_SOURCE) | $(OBJDIR)/
+$(LIBCC_OBJECT): $(LIBCC_SOURCE) | $(OBJDIR)/$(LIBCC_PREFIX)/
 	@$(LOG) Compiling $<
 	@$(CC) $(CFLAGS) -c $< -o $@
 
